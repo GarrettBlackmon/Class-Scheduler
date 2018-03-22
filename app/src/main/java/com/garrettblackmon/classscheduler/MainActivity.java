c1ae6cc7973f7d9c5bef6052698ece466699b7a7
Garrett <garrettsoumon@gmail.com>
2018-03-21 20:23:34 -0500

    Added functionality to Light Theme and Dark Theme option menu buttons. (Does not currently save state!)

--- app/src/main/java/com/garrettblackmon/classscheduler/MainActivity.java
+++ app/src/main/java/com/garrettblackmon/classscheduler/MainActivity.java
@@ -1,18 +1,26 @@
 package com.garrettblackmon.classscheduler;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkTheme);
+        }
+        else setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -26,12 +34,22 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.DarkTheme);
+            Toast.makeText(getApplicationContext(),"Dark Theme Applied!",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            setTheme(R.style.AppTheme);
+            Toast.makeText(getApplicationContext(),"Light Theme Applied!",Toast.LENGTH_SHORT).show();
+        }
         return true;
     }
 
@@ -44,15 +62,23 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_dark_theme) {
-            //set theme to light
+            //set theme to dark
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            restartApp();
             return true;
         }
         if (id == R.id.action_light_theme) {
-            //set theme to dark
+            //set theme to light
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            restartApp();
             return true;
         }
-
-
         return super.onOptionsItemSelected(item);
+    }
+
+    public void restartApp() {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
